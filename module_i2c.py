67d803ab3c3272812f234f5493ca9e5aac2b7259
Norbert.Schwarz <info@datentechnikschwarz.de>
2023-04-21 23:36:15 +0200

    Init

--- module_i2c.py
+++ module_i2c.py
@@ -11,18 +11,23 @@ class GPIO:
     def __init__(self):
         i2c = I2C(0, scl=Pin(21), sda=Pin(20))
         self.mcp = MCP23017(i2c, 0x20)
+        self.input = False
+        self.output = False
 
     def get_input(self, pin):
-        return self.mcp.pin(pin, mode=1)
+        self.input = self.mcp.pin(pin, mode=1, pullup=True)
+        return self.input
 
     def set_output(self, pin, state):
         self.mcp.pin(pin, mode=0, value=state)
+        return self.output
 
 
 # -----------------------------------------------------------------------------
 def main():
-    gpio = GPIO
-    print(gpio.get_input(0))
+    gpio = GPIO()
+    print(gpio.set_output(0, 1))
+    print(gpio.get_input(1))
 
 
 # ------------------------------------------------------------------------------
